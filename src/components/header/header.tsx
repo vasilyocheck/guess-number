80188d6559d0f2c906b8f9b22b9d076f1d2cb90b
vasilyocheck <vasily.medvedev1986@gmail.com>
2024-03-17 21:38:45 +0300

    refactor range

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -5,16 +5,11 @@ import { getRangeEnd, getRangeStart } from '@/utils/selectors'
 import s from './header.module.scss'
 
 export const Header = () => {
-  const start = makeNumReadable(useAppSelector(getRangeStart))
-  const end = makeNumReadable(useAppSelector(getRangeEnd))
-
   return (
     <header className={s.header}>
       <div>Привет, кожаный!</div>
       <div>Загадай любое число</div>
-      <div>
-        от {start} до {end} и я отгадаю его менее чем за 21 попытку
-      </div>
+      <div>от 1 до 1 000 000 и я отгадаю его менее чем за 21 попытку</div>
     </header>
   )
 }
